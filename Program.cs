97c4a9e4c1ed0e31b1551b50ea1864c332988699
dacevedom <d.acevedom1993@gmail.com>
2025-08-03 20:49:16 -0400

    Added app subscription example to C# Test App

--- Program.cs
+++ Program.cs
@@ -35,42 +35,42 @@
 
 // Activity 4
 
-Random dice = new Random();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// Random dice = new Random();
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
 // int roll1 = 6;
 // int roll2 = 6;
 // int roll3 = 6;
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total.");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles! +2 bonus to total.");
-        total += 2;   
-    }
-}
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples! +6 bonus to total.");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles! +2 bonus to total.");
+//         total += 2;   
+//     }
+// }
 
 
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
 
-else
-{
-    Console.WriteLine("Sorry, you lose.");
-}
+// else
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
 
 // End of Activity 4
 
@@ -86,3 +86,31 @@ else
 // }
 
 // End of Activity 5
+
+// Activity 6
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine("Renew now and save 20%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+    Console.WriteLine("Renew now and save 10%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+Console.Write($"Discount percentage: {discountPercentage}%");
